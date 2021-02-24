3753efb0b103ed4ea68184d8f82938bc5ca68ab1
yuanjiwei <yuanjw09@chinatelecom.cn>
2021-02-24 16:52:06 +0800

    add(throttle): 防抖

--- programming/throttle/throttle.js
+++ programming/throttle/throttle.js
@@ -1,15 +1,14 @@
-let container = document.getElementById('container')
 let counter = 0
-container.innerHTML = counter
+
 function addCounter() {
   container.innerHTML = ++counter
 }
-container.addEventListener('mousemove', throttle(addCounter, 1000))
 
+let container = document.getElementById('container')
+container.innerHTML = counter
+container.addEventListener('mousemove', throttle(addCounter, 1000, {tailing:true, heading: false}))
 
-
-//
-
+// 面试的时候要安装这种顺序一次写出比较好
 
 // v1 时间戳 头执行
 
@@ -49,7 +48,7 @@ function throttle(fn, wait) {
   }
 }
 
-// v3 首尾皆执行
+// v3 首尾皆执行 => 面试中要写出的水平
 
 function throttle(fn, wait) {
   let previous = 0
@@ -60,9 +59,9 @@ function throttle(fn, wait) {
     let args = arguments
 
     let now = +new Date()
-    let remaining = wait - (now - previous)
+    let remaining = wait - (now - previous) // now - previous >= 0  === remaining <=0
 
-    if(now - previous >= wait) {
+    if(now - previous >= wait) { // now < previous => 更改了系统时间 
       if(timeout) {
         clearTimeout(timeout)
         timeout = null
@@ -81,4 +80,48 @@ function throttle(fn, wait) {
 }
 
 
-// v4 option (heading, tailing)
+// v4 option (heading, tailing) => 要掌握的水平 
+// 三种情况 
+// 1. heading = true tailing = true
+// 2. heading = true tailing  = false
+// 3. heading = false tailing = true
+// Note: heading 和 tailing 至少有一个 true
+
+// heading = false 头不执行 -> 取消基于时间戳的机制 -> 走定时器的机制
+// tailing = true 尾不执行 -> 取消基于定时器的机制 -> 走时间戳的机制
+
+function throttle(fn, wait, options = { heading: true, tailing: true}) {
+  if(!options.heading && !options.tailing) throw new Error('throttle 函数 options配置不能全为false')
+
+  let previous = 0
+  let timeout
+
+  return function() {
+    let context = this
+    let args = arguments
+    let now = +new Date()
+
+    if(options.heading === false) {  // 控制时间戳 一种技巧
+      previous = now
+    }
+    let remaining = wait - (now - previous)
+
+    if(now - previous >= wait) { 
+      if(timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      fn.apply(context, context)
+
+    } else if(!timeout && options.tailing === true) { // 控制定时器
+      timeout = setTimeout(function() {
+        previous = +new Date()
+        fn.apply(context, context)
+        timeout = null
+      }, remaining)
+    }
+  }
+}
+
+// v5 返回值 具名函数 取消方法
