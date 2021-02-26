801c95ac79fe78517357079b58a01412e33a8ade
Syncher Pylon, Peng <syncher.pylon.peng@sap.com>
2021-02-26 22:10:19 +0800

    feat: update slides

--- programming/scope-and-closure/0x02-closure-demo.js
+++ programming/scope-and-closure/0x02-closure-demo.js
@@ -1,6 +1,8 @@
 function getSum(a, b) {
     const fact = 1;
-    return function() {
+    let c = 1234;
+    return function sum() {
+        // sum.all - sum.vo => {fact, a, b} - {} => {fact, a, b}
         return fact + a + b;
     }
 }
@@ -9,3 +11,9 @@ function getSum(a, b) {
 const f = getSum(100, 200);
 const res = f();
 console.log(res);
+
+
+(() => {
+    // no bind this
+    console.log(this);
+})()
---
+++ programming/scope-and-closure/0x03-function-scope.js
@@ -0,0 +1,12 @@
+let g = 1;
+
+function a(x) {
+    let y = 1;
+    return x + y;
+}
+
+b();
+if (g) {
+    let x = 3;
+    a(x);
+}
